f88c21b8446f586268fbcaa4a22ccf550a6fc243
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-12 00:35:44 -0300

    add all variable for fonts, colors and space

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -7,23 +7,43 @@ export const GlobalStyle = createGlobalStyle`
 
     :root {
         // Fonts
-        --font-extra-large: ${progressiveClamp(32, 48)}; 
-        --font-large: ${Rem(32)};
-        --font-medium: ${progressiveClamp(28, 32)};
-        --font-small: ${progressiveClamp(16, 20)};
-        --font-extra-small: ${Rem(12)};
+        --font-extra-large: ${progressiveClamp(40, 150)}; 
+        --font-large: ${progressiveClamp(40, 72)}; 
+        --font-medium: ${progressiveClamp(28, 40)};
+        --font-small: ${progressiveClamp(28, 24)};
+        --font-extra-small: ${Rem(24)};
+        --font-text-standard: ${progressiveClamp(15, 16)};
+        --font-navigation: ${Rem(12)};
 
         // Colors
 
+        --dark-cyan: #0E8784;
+        --dark-grey-blue: #333D4B;
+        --pale-orange: #FDD6BA;
+        --light-gream: #FEFCF7;
+        --grey: #83888F;
+
+        // Spacing
+
+        --spacing-container-padding: ${progressiveClamp(24, 80)};
+
     }
 
-
     body {
-    
+        font-family: 'Barlow', sans-serif;
+        font-size: var(--font-small);
+        background-color: var(--light-gream);
+    }
 
     #__next {
-        width: 1000px;
+        max-width: ;
         background-color: red;
+    }
+
+    h1, h2, h3, h4, h5, h6 { 
+        font-family: 'Fraunces', serif;
+        font-weight: black;
+        font-size: ${progressiveClamp(40, 72)}
     }
 
     button {
