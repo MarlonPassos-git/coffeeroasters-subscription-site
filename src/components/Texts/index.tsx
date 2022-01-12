d00eaefaf37f70f541f9e561eac4c7c176c7a0d9
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-12 01:25:06 -0300

    add text components

---
+++ src/components/Texts/index.tsx
@@ -0,0 +1,64 @@
+import { _H1, _H2, _H3, _TitleAlternateBig, _H4, _Paragraph } from "./texts-styles";
+import { ReactChildren, ReactChild } from "react";
+
+
+interface childrenProps { 
+    children: ReactChild | ReactChildren;
+}
+
+
+export function TitleAlternateBig({ children }: childrenProps) { 
+
+    return (
+        <_TitleAlternateBig>
+            { children }
+        </_TitleAlternateBig>
+    )
+}
+
+
+export function H1({ children }: childrenProps) { 
+
+    return (
+        <_H1>
+            { children }
+        </_H1>
+    )
+}
+
+export function H2({ children }: childrenProps) { 
+    
+        return (
+            <_H2>
+                { children }
+            </_H2>
+        )
+}
+
+export function H3({ children }: childrenProps) { 
+    
+    return (
+        <_H3>
+            {children}
+        </_H3>
+    )
+}
+
+
+export function H4({ children }: childrenProps) {
+        
+        return (
+            <_H4>
+                {children}
+            </_H4>
+        )
+}
+
+export function Text({ children }: childrenProps) {
+            
+            return (
+                <_Paragraph>
+                    {children}
+                </_Paragraph>
+            )
+}
---
+++ src/components/Texts/texts-styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+
+export const _TitleAlternateBig = styled.strong`
+    font-size: var(--font-extra-large);
+
+`
+
+export const _H1 = styled.h1`
+    font-size: var(--font-large);
+
+`
+
+export const _H2 = styled.h2`
+    font-size: var(--font-medium);
+`
+
+export const _H3 = styled.h3`
+    font-size: var(--font-small);
+`
+
+export const _H4 = styled.h4`
+    font-size: var(--font-extra-small);
+`
+
+export const _Paragraph = styled.p`
+    font-size: var(--font-extra-small);
+`
