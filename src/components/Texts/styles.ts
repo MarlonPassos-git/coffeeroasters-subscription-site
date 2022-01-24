9aa8b914626c086b0d5238d1e2a0d2f13d7fe860
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-24 07:33:32 -0300

    ➕ componentes genericos de texto

--- src/components/Texts/styles.ts
+++ src/components/Texts/styles.ts
@@ -2,40 +2,42 @@ import { progressiveClamp, Rem } from 'progressive-size/dist'
 import styled from 'styled-components'
 
 
-export const _TitleAlternateBig = styled.strong`
+export const TitleAlternateBig = styled.strong`
     font-size: var(--font-extra-large);
     line-height: ${Rem(72)};
     text-align: center;
     color: var(--grey);
     text-transform: lowercase; 
+    font-weight: 900;
+    font-family: 'Fraunces', sans-serif;
 
 
 `
 
-export const _H1 = styled.h1`
+export const H1 = styled.h1`
     font-size: var(--font-large); 
     color: var(--light-gream);
 `
 
-export const _H2 = styled.h2`
+export const H2 = styled.h2`
     font-size: var(--font-medium);
     line-height: ${progressiveClamp(28, 48)};
 `
  
-export const _H3 = styled.h3`
+export const H3 = styled.h3`
     font-size: var(--font-small);
     line-height: ${progressiveClamp(32, 36)};
 
 `
 
-export const _H4 = styled.h4`
+export const H4 = styled.h4`
     font-size: var(--font-extra-small);
     line-height: ${Rem(32)};
     
 `
 
-export const _Paragraph = styled.p`
-    font-size: var(--font-extra-small);
+export const Paragraph = styled.p`
+    font-size: var(--font-text-standard);
     line-height: ${progressiveClamp(25, 26)};
 
 `
