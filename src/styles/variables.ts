cafc0d4ac66d55d129b52ab492d00bb1920240ef
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-24 07:34:34 -0300

    ➕ variaveis de breapoint

---
+++ src/styles/variables.ts
@@ -0,0 +1,11 @@
+export const breakpoints = {
+    mobile: '520px',
+    tablet: '900px',
+    desktop: '1440px',
+    _mobile: 520,
+    _tablet: 900,
+    _desktop: 1440,
+}
+
+
+export const colors = "alllooooo"
