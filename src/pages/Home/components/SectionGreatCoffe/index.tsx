05b08d17ec633b3575ad549416d6bebbe8cc89aa
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-24 12:39:36 -0300

    ➕componte 'section Great Coffee'

---
+++ src/pages/Home/components/SectionGreatCoffe/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from "../../../../components/Button";
+import { reponsiveBackground } from "../../../../utils/responsiveBackground";
+import { Container, Text, Title } from "./sectionGeatCoffe_styles";
+
+export function SectionGreatCoffe() {
+
+  return (
+    <Container>
+      <Title>
+        Great coffee<br />made simple.
+      </Title>
+      <Text>
+        Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.
+      </Text>
+      <Button>
+        Create your plan
+      </Button>
+    </Container>
+  )
+}
