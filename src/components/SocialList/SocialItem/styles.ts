a502e68b332e0eddad1a83f8c981555a9a937453
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-02-04 12:18:05 -0300

    ➕ Efeito de hover no itens rede social

--- src/components/SocialList/SocialItem/styles.ts
+++ src/components/SocialList/SocialItem/styles.ts
@@ -4,6 +4,13 @@ import styled from "styled-components";
 export const Container = styled.li`
   height: ${Rem(24)};
   width: ${Rem(24)};
+  cursor: pointer;
+
+
+  &:hover, &:focus-visible {
+    filter: brightness(0) saturate(100%) invert(86%) sepia(18%) saturate(684%) hue-rotate(325deg) brightness(102%) contrast(98%);
+  }
+
 
 `
 
